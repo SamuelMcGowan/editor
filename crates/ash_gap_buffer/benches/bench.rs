51335f2cca4b5c0734ed18ab1cde90956a0b6d12
Sam McGowan <the.sam.mcg@protonmail.com>
2023-11-18 16:35:15 +0000

    Another benchmark.

--- crates/ash_gap_buffer/benches/bench.rs
+++ crates/ash_gap_buffer/benches/bench.rs
@@ -5,11 +5,22 @@ fn main() {
     divan::main();
 }
 
-#[bench(min_time = 0.5)]
+#[bench(min_time = 0.25)]
 fn push(bencher: Bencher) {
     let mut buf = GapBuffer::new();
 
     bencher.bench_local(|| {
         buf.push(0);
+    })
+}
+
+#[bench]
+fn move_gap(bencher: Bencher) {
+    let mut buf = GapBuffer::new();
+    buf.push_slice(b"hello, world, how are you???");
+
+    bencher.bench_local(|| {
+        buf.set_gap(buf.len());
+        buf.set_gap(0);
     })
 }
