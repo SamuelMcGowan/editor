f40399abe27d130bcd90e110f2078cadbc2917f0
Sam McGowan <the.sam.mcg@protonmail.com>
2023-11-21 13:20:35 +0000

    Inline string functions.

--- src/str.rs
+++ src/str.rs
@@ -6,24 +6,29 @@ pub struct GapString {
 }
 
 impl GapString {
+    #[inline]
     pub const fn new() -> Self {
         Self {
             inner: GapBuffer::new(),
         }
     }
 
+    #[inline]
     pub fn capacity(&self) -> usize {
         self.inner.capacity()
     }
 
+    #[inline]
     pub fn len(&self) -> usize {
         self.inner.len()
     }
 
+    #[inline]
     pub fn is_empty(&self) -> bool {
         self.inner.is_empty()
     }
 
+    #[inline]
     pub fn push(&mut self, ch: char) {
         match ch.len_utf8() {
             1 => self.inner.push(ch as u8),
@@ -33,6 +38,7 @@ impl GapString {
         }
     }
 
+    #[inline]
     pub fn push_back(&mut self, ch: char) {
         match ch.len_utf8() {
             1 => self.inner.push_back(ch as u8),
@@ -42,14 +48,17 @@ impl GapString {
         }
     }
 
+    #[inline]
     pub fn push_str(&mut self, s: &str) {
         self.inner.push_slice(s.as_bytes());
     }
 
+    #[inline]
     pub fn push_str_back(&mut self, s: &str) {
         self.inner.push_slice_back(s.as_bytes());
     }
 
+    #[inline]
     pub fn pop(&mut self) -> Option<char> {
         let ch = self.front().chars().next_back()?;
         let new_len = self.inner.front_len() - ch.len_utf8();
@@ -57,6 +66,7 @@ impl GapString {
         Some(ch)
     }
 
+    #[inline]
     pub fn pop_back(&mut self) -> Option<char> {
         let ch = self.back().chars().next()?;
         let new_len = self.inner.back_len() - ch.len_utf8();
@@ -64,12 +74,14 @@ impl GapString {
         Some(ch)
     }
 
+    #[inline]
     pub fn set_gap(&mut self, index: usize) {
         assert!(index <= self.len(), "index out of bounds");
         assert!(self.is_char_boundary(index), "index not on char boundary");
         self.inner.set_gap(index);
     }
 
+    #[inline]
     pub fn is_char_boundary(&self, index: usize) -> bool {
         match self.inner.get(index) {
             None => index == self.len(),
@@ -77,22 +89,27 @@ impl GapString {
         }
     }
 
+    #[inline]
     pub fn front(&self) -> &str {
         unsafe { std::str::from_utf8_unchecked(self.inner.front()) }
     }
 
+    #[inline]
     pub fn back(&self) -> &str {
         unsafe { std::str::from_utf8_unchecked(self.inner.back()) }
     }
 
+    #[inline]
     pub fn front_mut(&mut self) -> &mut str {
         unsafe { std::str::from_utf8_unchecked_mut(self.inner.front_mut()) }
     }
 
+    #[inline]
     pub fn back_mut(&mut self) -> &mut str {
         unsafe { std::str::from_utf8_unchecked_mut(self.inner.back_mut()) }
     }
 
+    #[inline]
     pub fn front_and_back_mut(&mut self) -> (&mut str, &mut str) {
         let (front, back) = self.inner.front_and_back_mut();
         unsafe {
@@ -102,10 +119,12 @@ impl GapString {
         }
     }
 
+    #[inline]
     pub fn clear(&mut self) {
         self.inner.clear();
     }
 
+    #[inline]
     pub fn truncate_front(&mut self, len: usize) {
         assert!(
             self.front().is_char_boundary(len),
@@ -115,6 +134,7 @@ impl GapString {
         self.inner.truncate_front(len);
     }
 
+    #[inline]
     pub fn truncate_back(&mut self, len: usize) {
         let len = self.inner.back_len().min(len);
         let index = self.inner.back_len() - len;
@@ -127,35 +147,42 @@ impl GapString {
         self.inner.truncate_back(len);
     }
 
+    #[inline]
     pub fn reserve(&mut self, additional: usize) {
         self.inner.reserve(additional);
     }
 
+    #[inline]
     pub fn shrink_to_fit(&mut self) {
         self.inner.shrink_to_fit();
     }
 
+    #[inline]
     pub fn shrink_to(&mut self, capacity: usize) {
         self.inner.shrink_to(capacity);
     }
 
+    #[inline]
     fn from_buffer_unchecked(inner: GapBuffer) -> Self {
         Self { inner }
     }
 }
 
 impl From<String> for GapString {
+    #[inline]
     fn from(value: String) -> Self {
         Self::from_buffer_unchecked(value.into_bytes().into())
     }
 }
 
 impl From<&str> for GapString {
+    #[inline]
     fn from(value: &str) -> Self {
         Self::from_buffer_unchecked(value.as_bytes().into())
     }
 }
 
+#[inline]
 fn is_utf8_char_boundary(byte: u8) -> bool {
     // Taken from std::is_char_boundary
     (byte as i8) >= -0x40
