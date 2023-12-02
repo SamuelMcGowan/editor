5c3e3ff70af8f76a5c2e89f7b92273000b216991
Sam McGowan <the.sam.mcg@protonmail.com>
2023-12-02 16:49:21 +0000

    Server skeleton.

--- crates/ash_server/src/main.rs
+++ crates/ash_server/src/main.rs
@@ -1,34 +1,36 @@
+use std::fs::{File, OpenOptions};
+use std::io::{ErrorKind, Write};
 use std::net::{TcpListener, TcpStream};
 
 use anyhow::{Context, Result};
-use ash_common::Request;
-use serde::Deserialize;
-use serde_json::Deserializer;
+use directories::ProjectDirs;
 
-const LOCALHOST: &str = "127.0.0.1:0";
+pub const LOCALHOST: &str = "127.0.0.1:0";
 
 fn main() -> Result<()> {
     init_logging()?;
 
-    let listener = TcpListener::bind(LOCALHOST).context("couldn't bind to socket")?;
+    let project_dirs =
+        ProjectDirs::from("", "", "ash_editor").context("couldn't get project directories")?;
 
-    for stream in listener.incoming() {
-        let stream = match stream {
-            Ok(stream) => stream,
-            Err(err) => {
-                log::error!("connection failed: {err}");
-                continue;
-            }
-        };
+    std::fs::create_dir_all(project_dirs.data_dir())
+        .context("couldn't create session data directory")?;
 
-        std::thread::spawn(|| {
-            if let Err(err) = handle_connection(stream) {
-                log::error!("{err}");
-            }
-        });
+    let session_file_path = project_dirs.data_dir().join("session");
+
+    match OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(&session_file_path)
+    {
+        Ok(file) => {
+            let res = run_server(file);
+            std::fs::remove_file(session_file_path).context("couldn't delete session file")?;
+            res
+        }
+        Err(err) if err.kind() == ErrorKind::AlreadyExists => Ok(()),
+        Err(err) => Err(anyhow::Error::from(err).context("couldn't create session file")),
     }
-
-    Ok(())
 }
 
 fn init_logging() -> Result<()> {
@@ -45,21 +47,40 @@ fn init_logging() -> Result<()> {
             ))
         })
         .level(log::LevelFilter::Debug)
-        .chain(std::io::stderr())
-        .chain(fern::log_file("output-server.log")?)
+        .chain(fern::log_file("output.log")?)
         .apply()?;
 
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<()> {
-    #[allow(clippy::never_loop)]
-    loop {
-        let mut de = Deserializer::from_reader(&mut stream);
-        let request = Request::deserialize(&mut de)?;
+struct Guard<F: FnMut()>(F);
 
-        match request {
-            Request::Quit => return Ok(()),
-        }
+impl<F: FnMut()> Drop for Guard<F> {
+    fn drop(&mut self) {
+        self.0()
     }
+}
+
+fn run_server(mut session_file: File) -> Result<()> {
+    let listener = TcpListener::bind(LOCALHOST).context("couldn't bind to socket")?;
+    let addr = listener
+        .local_addr()
+        .context("couldn't get socket address")?;
+
+    write!(session_file, "{}", addr.port()).context("couldn't write port to session file")?;
+
+    for stream in listener.incoming() {
+        let stream = stream.context("connection failed")?;
+        std::thread::spawn(|| {
+            if let Err(err) = handle_connection(stream) {
+                log::error!("{}", err.context("while handling connection"));
+            }
+        });
+    }
+
+    Ok(())
+}
+
+fn handle_connection(_stream: TcpStream) -> Result<()> {
+    Ok(())
 }
