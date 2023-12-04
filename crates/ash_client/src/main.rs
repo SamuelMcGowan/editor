d260c940d46b5f2c2457fb5884b276a50b1c097f
Sam McGowan <the.sam.mcg@protonmail.com>
2023-12-04 13:33:26 +0000

    Create `Client` type.

--- crates/ash_client/src/main.rs
+++ crates/ash_client/src/main.rs
@@ -1,10 +1,10 @@
-use std::io::Write;
-use std::net::TcpStream;
+use std::io::{self};
+use std::net::{TcpStream, ToSocketAddrs};
 
 use anyhow::{Context, Result};
 use ash_server::{Request, Response};
-use serde::Deserialize;
-use serde_json::Deserializer;
+use serde::{Deserialize, Serialize};
+use serde_json::{Deserializer, Serializer};
 
 fn main() -> Result<()> {
     init_logging()?;
@@ -15,20 +15,9 @@ fn main() -> Result<()> {
     let port = std::fs::read_to_string(session_file_path).context("couldn't read session file")?;
     let port = port.parse::<u16>()?;
 
-    let mut stream =
-        TcpStream::connect(("127.0.0.1", port)).context("couldn't connect to server")?;
+    let mut client = Client::new(("localhost", port))?;
 
-    log::info!("connected to server at {}", stream.peer_addr()?);
-
-    let request = serde_json::to_string(&Request::Quit)?;
-    log::info!("request: {request}");
-
-    write!(stream, "{request}")?;
-    stream.flush()?;
-
-    let mut de = Deserializer::from_reader(stream);
-    let response = Response::deserialize(&mut de).context("couldn't read response")?;
-
+    let response = client.send(Request::Quit)?;
     log::info!("response: {response:?}");
 
     Ok(())
@@ -53,4 +42,37 @@ fn init_logging() -> Result<()> {
         .apply()?;
 
     Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum ClientError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("JSON error: {0}")]
+    Json(#[from] serde_json::Error),
+}
+
+pub type ClientResult<T> = Result<T, ClientError>;
+
+pub struct Client {
+    write: Serializer<TcpStream>,
+    read: Deserializer<serde_json::de::IoRead<TcpStream>>,
+}
+
+impl Client {
+    pub fn new(addr: impl ToSocketAddrs) -> ClientResult<Self> {
+        let stream = TcpStream::connect(addr)?;
+        let stream2 = stream.try_clone()?;
+
+        Ok(Self {
+            write: Serializer::new(stream),
+            read: Deserializer::from_reader(stream2),
+        })
+    }
+
+    pub fn send(&mut self, request: Request) -> ClientResult<Response> {
+        request.serialize(&mut self.write)?;
+        Ok(Response::deserialize(&mut self.read)?)
+    }
 }
